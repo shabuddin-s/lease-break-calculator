2ea5213bd2b9d09a34f66e5e12a43ec47612906b
shabuddin <shabuddin.s@gmail.com>
2026-04-29 10:30:41 -0500

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://earlyleaseterminationcalculator.com',
   trailingSlash: 'never',
+  integrations: [sitemap()],
 });
